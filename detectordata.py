c300d5c70a6d17bf3d5aaaafc08373cd40199875
Mi Yon Kim <miyon@pdx.edu>
2020-12-01 12:59:48 -0800

    Add documentation

--- detectordata.py
+++ detectordata.py
@@ -1,7 +1,9 @@
 from csv import DictReader
 from json import dump
 
+# final list of detectors
 detectors = list()
+
 stations = dict()
 highways = dict()
 
@@ -12,21 +14,23 @@ with open("freeway_detectors.csv", "r", encoding='UTF-8' ) as csvfile:
 with open("freeway_stations.csv", "r", encoding='UTF-8' ) as csvfile:
     reader = DictReader(csvfile)
     s_list = list(reader)
+    # Make dictionary of dictionary: stations[s_id][key] = value
     for s in s_list:
         stations[s["stationid"]] = dict()
         stations[s["stationid"]]["stationid"] = s["stationid"]
         stations[s["stationid"]]["upstream"] = s["upstream"]
         stations[s["stationid"]]["downstream"] = s["downstream"]
         stations[s["stationid"]]["stationclass"] = s["stationclass"]
-        stations[s["stationid"]]["numberlanes"] = s["numberlanes"]
+        stations[s["stationid"]]["numberlanes"] = int(s["numberlanes"])
         stations[s["stationid"]]["latlon"] = s["latlon"]
-        stations[s["stationid"]]["length"] = s["length"]
+        stations[s["stationid"]]["length"] = float(s["length"])
 
 
 
 with open("highways.csv", "r", encoding='UTF-8' ) as csvfile:
     reader = DictReader(csvfile)
     h_list = list(reader)
+    # Make dictionary of dictionary: highways[h_id][key] = value
     for h in h_list:
         highways[h["highwayid"]] = dict()
         highways[h["highwayid"]]["highwayid"] = h["highwayid"]
@@ -36,10 +40,14 @@ with open("highways.csv", "r", encoding='UTF-8' ) as csvfile:
 
 
 for d in detectors:
+    d["schema"] = "1.0"
+    # Add station data to each detector
     d["station"] = stations[d["stationid"]]
     del d['stationid']
+    # Add highway data to each detector
     d["highway"] = highways[d["highwayid"]]
     del d['highwayid']
+    d["lanenumber"] = int(d["lanenumber"])
 
-with open("a.json", 'w') as f:
+with open("detector.json", 'w') as f:
     dump(detectors, f)
