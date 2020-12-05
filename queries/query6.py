9404e5645301bc33e49b088686b0dee6e389ad0c
Mi Yon Kim <miyon@pdx.edu>
2020-12-05 00:37:55 -0800

    Change the display format

--- queries/query6.py
+++ queries/query6.py
@@ -4,7 +4,6 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 load_dotenv()
 MONGO_HOST = os.getenv("MONGO_HOST")
-
 MONGO_DB = os.getenv("MONGO_DB")
 
 # connect to cluster
@@ -15,14 +14,15 @@ db = cluster[MONGO_DB]
 col_detectors = db["Detectors"]
 
 results = col_detectors.find({"locationtext": "Foster NB"})
+milepost = "18.1"
 
-print("Display the data before updating milepost")
+print("\n\n---- Display the data before updating milepost ----")
 for r in results:
-    print(r)
-    col_detectors.update_one({"milepost":r.get("milepost")}, {"$set":{"milepost": "18.1"}})
+    print("milepost:", r["milepost"])
+    col_detectors.update_one({"milepost":r.get("milepost")}, {"$set":{"milepost": milepost}})
 
 results = col_detectors.find({"locationtext": "Foster NB"})
 
-print("Display the data after updating milepost to 30.0")
+print("\n\n---- Display the data after updating milepost to " + milepost + " ----")
 for r in results:
-    print(r)
+    print("milepost", r["milepost"])
