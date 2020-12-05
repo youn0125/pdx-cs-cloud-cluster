4cb5440cfa94a54ac26bfcb17ef3d6b9332bc174
Mi Yon Kim <miyon@pdx.edu>
2020-12-05 00:31:33 -0800

    Implement query 6: updating the detectors data

--- queries/query6.py
+++ queries/query6.py
@@ -4,9 +4,11 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 load_dotenv()
 MONGO_HOST = os.getenv("MONGO_HOST")
+
 MONGO_DB = os.getenv("MONGO_DB")
 
-cluster = MongoClient(MONGO_HOST,27017)
+# connect to cluster
+cluster = MongoClient(MONGO_HOST, 27017)
 db = cluster[MONGO_DB]
 
 # Get collection for query
@@ -14,6 +16,13 @@ col_detectors = db["Detectors"]
 
 results = col_detectors.find({"locationtext": "Foster NB"})
 
+print("Display the data before updating milepost")
 for r in results:
     print(r)
+    col_detectors.update_one({"milepost":r.get("milepost")}, {"$set":{"milepost": "18.1"}})
 
+results = col_detectors.find({"locationtext": "Foster NB"})
+
+print("Display the data after updating milepost to 30.0")
+for r in results:
+    print(r)
